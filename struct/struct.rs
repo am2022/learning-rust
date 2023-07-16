ce101c43b37f2fdcc1ee16b5bc576badc58e0837
am <am2022a2@gmail.com>
2023-07-16 11:36:30 +0330

    user1 created from User struct

--- struct/struct.rs
+++ struct/struct.rs
@@ -1,5 +1,17 @@
 struct User{
     username: String,
     email: String,
-    friend_count: i32
+    friend_count: i32,
+}
+
+fn main(){
+    let user1 = User{
+        username: String::from("test@test.com"),
+        email: String::from("test"),
+        friend_count: 5,
+    };
+
+    println!("username: {}", user1.username);
+    println!("email: {}", user1.email);
+    println!("number of friend: {}", user1.friend_count);
 }
