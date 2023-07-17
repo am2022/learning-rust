c3927208aac8bc663a3fcb421794ff2cb789c945
am <am2022a2@gmail.com>
2023-07-17 16:07:50 +0330

    max function created

--- function/func.rs
+++ function/func.rs
@@ -2,6 +2,20 @@ fn hello(){
     println!("hello world!");
 }
 
+fn max(x: i64, y: i64) -> i64{
+    if x < y{
+        return y;
+    }
+    else{
+        return x;
+    }
+}
+
 fn main(){
+    let a: i64 = 16;
+    let b: i64 = 18;
+
     hello(); //prints hello world
+    max_ab = max(a, b); //returns bigger number
+    println!("max:{max_ab}");
 }
